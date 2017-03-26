226fab17742b67a775b4797e6c3678e9482b7e56
Ten <ten@chuwa.iobb.net>
2017-03-27 08:46:27 +0900

    Update jsxfunc.jsx

--- net.sytes.chuwa.callbacktest/jsx/jsxfunc.jsx
+++ net.sytes.chuwa.callbacktest/jsx/jsxfunc.jsx
@@ -1,1 +1,20 @@
-﻿/*jslint vars: true, plusplus: true, devel: true, nomen: true, regexp: true, indent: 4, maxerr: 50 *//*global $, Folder*/function testFunc() {try {	var xLib = new ExternalObject("lib:\PlugPlugExternalObject");	} catch (e) {alert(e);}	var eventObj = new CSXSEvent(); 	eventObj.type = "getCallback";	eventObj.data = "no selection";	if (app.activeDocument.selection.length>0){		var bnds = app.activeDocument.selection[0].geometricBounds;		eventObj.data = "x1:"+ bnds[0] + "\ny1:" + bnds[1] + "\nx2:"+ bnds[2] + "\ny2:" + bnds[3];		}	eventObj.dispatch();	xLib.unload();	return true;	}testFunc();
+/*jslint vars: true, plusplus: true, devel: true, nomen: true, regexp: true, indent: 4, maxerr: 50 */
+/*global $, Folder*/
+
+
+function testFunc() {
+try {
+	var xLib = new ExternalObject("lib:\PlugPlugExternalObject");
+	} catch (e) {alert(e);}
+	var eventObj = new CSXSEvent(); 
+	eventObj.type = "getCallback";
+	eventObj.data = "no selection";
+	if (app.activeDocument.selection.length>0){
+		var bnds = app.activeDocument.selection[0].geometricBounds;
+		eventObj.data = "x1:"+ bnds[0] + "\ny1:" + bnds[1] + "\nx2:"+ bnds[2] + "\ny2:" + bnds[3];
+		}
+	eventObj.dispatch();
+	xLib.unload();
+	return true;
+	}
+testFunc();
