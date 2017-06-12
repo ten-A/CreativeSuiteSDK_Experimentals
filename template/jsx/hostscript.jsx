34de356d68e1964d6852054d243f0b812445d2b9
Ten <ten@chuwa.iobb.net>
2017-06-12 13:44:45 +0900

    update...

--- template/jsx/hostscript.jsx
+++ template/jsx/hostscript.jsx
@@ -1,1 +1,8 @@
-function bmi(num) {	var bmi = num[1] / (num[0] * num[0]) * 10000;	var st = "";	if (bmi>30) st = " Club Over 30 ﾍﾖｳｺｿ";	else if (bmi<20) st = " ﾔｾｽｷﾞﾃﾞｽ､ﾓｳﾁｯﾄﾀﾍﾞﾏｾｳ";	else st = " ﾌﾂｳｽｷﾞﾃｵﾓｼﾛｸﾅｲﾈ"	alert("ｱﾅﾀﾉBIMｹｰｻﾝｼﾀﾈ\n"+ bmi + "");	}
+function bmi(num) {	
+  var bmi = num[1] / (num[0] * num[0]) * 10000;
+	var st = "";
+	if (bmi>30) st = " Club Over 30 ﾍﾖｳｺｿ";
+	else if (bmi<20) st = " ﾔｾｽｷﾞﾃﾞｽ､ﾓｳﾁｯﾄﾀﾍﾞﾏｾｳ";
+	else st = " ﾌﾂｳｽｷﾞﾃｵﾓｼﾛｸﾅｲﾈ"
+     alert("ｱﾅﾀﾉBIMｹｰｻﾝｼﾀﾈ\n"+ bmi + st);
+	}
