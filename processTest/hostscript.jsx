0facddcff53335c8af4364a1070d91679bd9285e
Ten <ten@chuwa.iobb.net>
2017-07-12 08:51:41 +0900

    Process test.
    How to make process from CEP Extensions.

---
+++ processTest/hostscript.jsx
@@ -0,0 +1,7 @@
+function func1(){
+    alert("Javascript function called.");
+}
+
+function func2(){
+    app.doAction("action1","ActionSet1");
+}
---
+++ processTest/main.js
@@ -0,0 +1,15 @@
+function createProess(){
+	var createProcessResult = window.cep.process.createProcess("/bin/ps", "x", "");
+	if (0 == createProcessResult.err) {
+		var gPID = createProcessResult.data;
+		var stdoutResult = window.cep.process.stdout(gPID, function(output) {
+			console.log(output);
+			console.log(output.match(/\s+?(\d+).+Illustrator/));
+			alert("Illustrators gPID :  " + RegExp.$1);
+			});
+		}
+	else {
+		alert("createProcess failed");
+		}
+	}
+
