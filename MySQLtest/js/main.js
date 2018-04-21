5cb350aadecbbc9cbad580b1be94443aa546305e
Ten <ten@chuwa.iobb.net>
2018-04-21 11:36:53 +0900

    Update main.js

--- MySQLtest/js/main.js
+++ MySQLtest/js/main.js
@@ -4,28 +4,26 @@ themeManager.init();
 function get() {
 	var mysql = require('mysql');
 	var connection = mysql.createConnection({
-	   host : '130.1.6.80',
-	   port : '3306',
-	   user : 'bookuser',
-	   password : 'bookuser1',
-	   database : 'book_db'
+	   host : '<MySQL_SERVER>',
+	   port : '<PORT_NUMBER>',
+	   user : '<DATABASE_USER>',
+	   password : '<USER_PASSWORD>',
+	   database : '<DATABASE_NAME>'
 	   });
 	connection.connect();
-	var queryString = "SELECT `order_id` AS `order_id`, `size` AS `size`, `quantity_order` AS `quantity_order`, "
-			+ "`post` AS `post`, `adds` AS `adds`, `name` AS `name`, `delivered` AS `delivered` "
-			+ "FROM `book_db`.`work_data` AS `work_data` WHERE `delivered` = 0 ORDER BY `size` ASC";	
+	var queryString = "SELECT `order` AS `order`, `size` AS `size`, "
+			+ "`name` AS `name`, `delivered` AS `delivered` "
+			+ "FROM `db`.`work_data` AS `work_data` WHERE `delivered` = 0 ORDER BY `size` ASC";	
 	connection.query(queryString, function(err, rows, fields) {
 	if (err) console.log(err);
 	else {
 		var str = "";
 		for (var i=0;i<rows.length;i++){
-			str += rows[i].order_id + "," + rows[i].size + "," +  rows[i].quantity_order + "," 
-			        +  rows[i].post + "," +  rows[i].adds + "," +  rows[i].name +"\r"
+			str += rows[i].order + "," + rows[i].size + "," +  rows[i].post + "," +  rows[i].name +"\r"
 			}
 		document.getElementById("text").value = str;
 		}
 	});
-
 	connection.end();
     };
 
@@ -33,5 +31,3 @@ function apply() {
     var csi = new CSInterface();
     csi.evalScript("makeLabel(" + document.getElementById("text").value + ")");
 }
-
-
