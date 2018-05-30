bfc9fad6332019c7494f29bd81cae2236fcb02a8
Ten <ten@chuwa.iobb.net>
2018-05-30 13:44:18 +0900

    upddate function

--- template/jsx/hostscript.jsx
+++ template/jsx/hostscript.jsx
@@ -1,8 +1,4 @@
-function bmi(num) {	
-  var bmi = num[1] / (num[0] * num[0]) * 10000;
-	var st = "";
-	if (bmi>30) st = " Club Over 30 ﾍﾖｳｺｿ";
-	else if (bmi<20) st = " ﾔｾｽｷﾞﾃﾞｽ､ﾓｳﾁｯﾄﾀﾍﾞﾏｾｳ";
-	else st = " ﾌﾂｳｽｷﾞﾃｵﾓｼﾛｸﾅｲﾈ"
-     alert("ｱﾅﾀﾉBIMｹｰｻﾝｼﾀﾈ\n"+ bmi + st);
+function callExtendscriptFunc(str) {	
+	alert("Alert from ExtendScript side : " + str);
+	return true;
 	}
